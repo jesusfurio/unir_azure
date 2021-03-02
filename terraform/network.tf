f9d6e6eea629b2352486e019c800ba810670fe8b
jesusfurio <jesus.furio.nunez@gmail.com>
2021-03-02 19:58:26 +0100

    Añadida regla NSG

--- terraform/network.tf
+++ terraform/network.tf
@@ -21,6 +21,17 @@ resource "azurerm_network_security_group" "unir_nsg" {
     name                = "${azurerm_virtual_network.unir_network.name}-192.168.1.0-24.nsg"
     location            = azurerm_resource_group.unir_rg.location
     resource_group_name = azurerm_resource_group.unir_rg.name
+    security_rule {
+        name = "ssh"
+        priority = 1001
+        direction = "Inbound"
+        access = "Allow"
+        protocol = "Tcp"
+        source_port_range = "*"
+        destination_port_range = "22"
+        source_address_prefix = "*"
+        destination_address_prefix = "*"
+  }
 }
 # Subnet association to NSG
 resource "azurerm_subnet_network_security_group_association" "unir_sb-net_asso" {
